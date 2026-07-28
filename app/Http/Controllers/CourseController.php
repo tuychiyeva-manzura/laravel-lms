41eeb72ddecd19a65fe4de12b87ff9edc293a57a
tuychiyeva-manzura <tuychiyevamanzura80@gmail.com>
2026-07-28 22:33:49 +0500

    Add Course index view and implement CourseController index method

--- app/Http/Controllers/CourseController.php
+++ app/Http/Controllers/CourseController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Course;
 
 class CourseController extends Controller
 {
@@ -11,54 +12,16 @@ class CourseController extends Controller
      */
     public function index()
     {
-        //
+        $course=Course::all();
+        return view('course.index',[
+            'course'=>$course
+        ]);
     }
-
+    }
     /**
      * Show the form for creating a new resource.
      */
-    public function create()
-    {
-        //
-    }
+  
 
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
-    }
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
-    {
-        //
-    }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
-    {
-        //
-    }
-}
---
+++ resources/views/course/index.blade.php
@@ -0,0 +1,8 @@
+
+@extends('layouts.app')
+@section('title', 'LMS| O'quv Markaz kurslari')
+@section('content')
+<div class="">
+    <h1> O'quv markaz kurslari</h1>
+    </div>
+@endsection
